b2c23a880051a016e8a27582e1d7507911e72456
Matti Luukkainen <mluukkai@iki.fi>
2024-03-18 13:44:06 +0200

    Update PokemonPage.jest.spec.jsx

--- test/PokemonPage.jest.spec.jsx
+++ test/PokemonPage.jest.spec.jsx
@@ -9,17 +9,6 @@ import { MemoryRouter } from 'react-router-dom'
 
 jest.mock('axios')
 
-const previous = {
-  url: 'https://pokeapi.co/api/v2/pokemon/132/',
-  name: 'ditto',
-  id: 132
-}
-const next = {
-  url: 'https://pokeapi.co/api/v2/pokemon/134/',
-  name: 'vaporeon',
-  id: 134
-}
-
 const pokemonList = {
   id: 133,
   abilities: [
