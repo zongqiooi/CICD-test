d8d3685112f2d926564af398038b384b3cf939d9
zongqiooi <huro710@hotmail.com>
2024-04-21 22:48:23 +0800

    fix: remove single quotes and semicolon rules from stylistic

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,6 @@ module.exports = {
   rules: {
     "@stylistic/js/indent": ["error", 2],
     "@stylistic/js/linebreak-style": ["error", "unix"],
-    "@stylistic/js/quotes": ["error", "single"],
-    "@stylistic/js/semi": ["error", "never"],
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "single"],
